2217a6f02a811b3b1001873f5280e1e80b35e31a
Jonathan Ziller <jonathan.ziller@gmail.com>
2015-12-17 10:35:30 +0100

    Exercise 12: Faulty Waiter

--- ScalaCoffeeShopFSharp/Program.fs
+++ ScalaCoffeeShopFSharp/Program.fs
@@ -5,49 +5,81 @@ open Actor
 open Akka.Actor
 open Akka.FSharp
 
-type Coffee =
-| Akkaccino
-| MochaPlay
-| CaffeScala
+module Coffee =
+  type T =
+  | Akkaccino
+  | MochaPlay
+  | CaffeScala
+
+  let private getRandArrElement =
+    let rnd = Random()
+    fun (arr : T[]) -> arr.[rnd.Next(arr.Length)]
+
+  let anyOther c =
+    Set.ofList [Akkaccino; MochaPlay; CaffeScala]
+    |> Set.remove c
+    |> Array.ofSeq
+    |> getRandArrElement
 
 module Message =
   type CoffeeHouse =
-  | CreateGuest of favCoffee: Coffee * caffeineLimit: int
-  | ApproveCoffee of coffee: Coffee * guest: IActorRef
+  | CreateGuest of favCoffee: Coffee.T * caffeineLimit: int
+  | ApproveCoffee of coffee: Coffee.T * guest: IActorRef
 
   type Guest =
-  | CoffeeServed of coffee: Coffee
+  | CoffeeServed of coffee: Coffee.T
   | CoffeeFinished
 
   type Waiter =
-  | ServeCoffee of coffee: Coffee
-  | CoffeePrepared of coffee: Coffee * guest: IActorRef
+  | ServeCoffee of coffee: Coffee.T
+  | CoffeePrepared of coffee: Coffee.T * guest: IActorRef
+  | Complaint of coffee: Coffee.T
 
   type Barista =
-  | PrepareCoffee of coffee: Coffee * guest: IActorRef
+  | PrepareCoffee of coffee: Coffee.T * guest: IActorRef
 
 module Waiter =
   open Message
 
-  let create system name coffeeHouse =
-    spawn system name (typedActorOf2 (fun mailbox msg ->
+  type FrustratedException() =
+    inherit InvalidOperationException("Waiter is frustrated!")
+
+  let create system name coffeeHouse barista maxComplaintCount =
+    spawn system name (typedActorOf3 (fun mailbox complaintCount msg ->
       match msg with
-      | ServeCoffee c -> coffeeHouse <! ApproveCoffee (c, mailbox.Sender())
-      | CoffeePrepared(c, g) -> g <! Guest.CoffeeServed c
-    ))
+      | ServeCoffee c -> 
+        coffeeHouse <! ApproveCoffee (c, mailbox.Sender())
+        complaintCount
+      | CoffeePrepared(c, g) -> 
+        g <! Guest.CoffeeServed c
+        complaintCount
+      | Complaint(c) when complaintCount >= maxComplaintCount ->
+        raise <| new FrustratedException()
+      | Complaint(c) ->
+        barista <! PrepareCoffee(c, mailbox.Sender())
+        complaintCount + 1
+    ) 0)
 
 module Barista =
   open Message
 
-  let create system name (prepareCoffeeDuration: TimeSpan) =
-    spawn system name (typedActorOf2 (fun mailbox msg ->
-      match msg with
-      | PrepareCoffee (c, g) -> 
-        mailbox.Sender() <! Async.RunSynchronously(async {
-          do! Async.Sleep (int prepareCoffeeDuration.TotalMilliseconds)
-          return CoffeePrepared(c, g)
-        })
-    ))
+  let create system name (prepareCoffeeDuration: TimeSpan) accuracy =
+    let run (mailbox: Actor<obj>) =
+      let r = new Random()
+
+      let run _ msg =
+        match msg with
+        | PrepareCoffee (c, g) -> 
+          let preparedCoffee = if r.Next 100 < accuracy then c else Coffee.anyOther c
+          
+          mailbox.Sender() <! Async.RunSynchronously(async {
+            do! Async.Sleep (int prepareCoffeeDuration.TotalMilliseconds)
+            return CoffeePrepared(preparedCoffee, g)
+          })
+
+      typedActorOf2 run mailbox
+
+    spawn system name run
 
 module Guest =
   open Message
@@ -64,6 +96,10 @@ module Guest =
 
         let run mailbox coffeeCount msg =
           match msg with
+          | CoffeeServed c when c <> favCoffee ->
+            Logging.logInfof mailbox "Expected a %A, but got a %A!" favCoffee c
+            waiter <! Complaint(favCoffee)
+            coffeeCount
           | CoffeeServed c -> 
             let newCoffeeCount = coffeeCount + 1
             Logging.logInfof mailbox "Enjoying my %d yummy %A!" newCoffeeCount c
@@ -90,9 +126,11 @@ module CoffeeHouse =
     
       let finishCoffeeDuration = mailbox.Context.System.Settings.Config.GetTimeSpan "coffee-house.guest.finish-coffee-duration"
       let prepareCoffeeDuration = mailbox.Context.System.Settings.Config.GetTimeSpan "coffee-house.barista.prepare-coffee-duration"
+      let baristaAccuracy = mailbox.Context.System.Settings.Config.GetInt "coffee-house.barista.accuracy"
+      let maxComplaintCount = mailbox.Context.System.Settings.Config.GetInt "coffee-house.waiter.max-complaint-count"
 
-      let barista = Barista.create mailbox.Context "barista" prepareCoffeeDuration
-      let waiter = Waiter.create mailbox.Context "waiter" mailbox.Self
+      let barista = Barista.create mailbox.Context "barista" prepareCoffeeDuration baristaAccuracy
+      let waiter = Waiter.create mailbox.Context "waiter" mailbox.Self barista maxComplaintCount
 
       Logging.logInfo mailbox "CoffeeHouse Open"
 
@@ -134,8 +172,11 @@ module CoffeeHouse =
     spawnOpt system "coffee-house" run [
       SpawnOption.SupervisorStrategy(Strategy.OneForOne decider)
     ]
+    
+open Coffee
 
 let run() =
+
   let config = Configuration.load()
   use system = System.create "my-system" config
   let caffeineLimit = system.Settings.Config.GetInt "coffee-house.caffeine-limit"
@@ -143,7 +184,7 @@ let run() =
   let coffeeHouse = CoffeeHouse.create system caffeineLimit
 
   coffeeHouse <! Message.CreateGuest(Akkaccino, 2)
-  coffeeHouse <! Message.CreateGuest(CaffeScala, 2)
+  // coffeeHouse <! Message.CreateGuest(CaffeScala, 2)
 
   Console.ReadKey() |> ignore
 
